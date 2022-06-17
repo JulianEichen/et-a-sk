9e44c978f693e3d995f0b5021c6f55459a318205
Julian <juleichen@aol.com>
2022-06-17 16:04:23 +0200

    colors changing on hover

--- etch.js
+++ etch.js
@@ -1,5 +1,15 @@
 const container = document.querySelector('.container');
 
+const grid_size = 16*16;
+
+for (var i=0; i<grid_size;i++){
+    const div = document.createElement('div');
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'white';
+    });
+    container.appendChild(div);
+}
+
 
 
 
