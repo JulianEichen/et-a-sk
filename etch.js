4ce8b0878c01626213f52b3dca4806d28fb75f9d
Julian <juleichen@aol.com>
2022-06-27 14:36:31 +0200

    fixed initialization bug

--- etch.js
+++ etch.js
@@ -1,11 +1,12 @@
 const grid = document.querySelector('.grid');
 
 // Create the default grid
+document.documentElement.style.setProperty('--grid_brush_color', 'white')
 create_grid();
 
 function create_grid(length=getComputedStyle(document.documentElement).getPropertyValue('--grid_length')){
     const grid_size = length*length;
-
+    color = getComputedStyle(document.documentElement).getPropertyValue('--grid_brush_color');
     // cleare grid
     const current_squares = document.querySelectorAll('.square');
     current_squares.forEach((square) => {
@@ -19,8 +20,9 @@ function create_grid(length=getComputedStyle(document.documentElement).getProper
     for (var i = 0; i < grid_size; i++) {
         const div = document.createElement('div');
         div.className = "square";
+        
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'white';
+            div.style.backgroundColor = color;
         })
         grid.appendChild(div);
     }
@@ -33,20 +35,8 @@ change_size.addEventListener('click', resize_grid);
 const toggle_grid = document.querySelector('.toggle_grid');
 toggle_grid.addEventListener('click', toggle_borders);
 
-const select_brush = document.querySelector('.drop_button');
-select_brush.addEventListener('click', toggle_drop);
+document.getElementById("dropdown").addEventListener('change', select_brush);
 
-const brush_earth = document.querySelector('.brush_earth');
-brush_earth.addEventListener('click', toggle_drop);
-
-const brush_plant = document.querySelector('.brush_plant');
-brush_plant.addEventListener('click', change_plant() =>);
-
-const brush_water = document.querySelector('.brush_water');
-brush_water.addEventListener('click', change_brush);
-
-const brush_rock = document.querySelector('.brush_rock');
-brush_rock.addEventListener('click', change_brush);
 
 // Menu functions
 function resize_grid() {
@@ -57,7 +47,6 @@ function resize_grid() {
 
 function toggle_borders(){
     const style = getComputedStyle(document.documentElement).getPropertyValue('--grid_border_style');
-    console.log(style);
     if (getComputedStyle(document.documentElement).getPropertyValue('--grid_border_style') == "solid"){
         document.documentElement.style.setProperty('--grid_border_style', "hidden");
     }else{
@@ -65,14 +54,10 @@ function toggle_borders(){
     }
 }
 
-function toggle_drop(){
-    document.getElementById("myDrop").classList.toggle("show");
-}
 
-function change_brush(color){
-    console.log("change_brush");
-    toggle_drop();
-    document.documentElement.style.setProperty('--grid_brush_color', color)
+function select_brush(){
+    var selectedOption = this.options[this.selectedIndex].value;
+    document.documentElement.style.setProperty('--grid_brush_color', selectedOption);
     
     // change listeners
     const current_squares = document.querySelectorAll('.square');
@@ -80,12 +65,14 @@ function change_brush(color){
         square.removeEventListener('mouseover', () => {
             div.style.backgroundColor = 'white';
         });
-        /*
+        
         square.addEventListener('mouseover', () => {
-            div.style.backgroundColor = color;
-        }); */
+            square.style.backgroundColor = selectedOption;
+        }); 
     })
+    
 }
+
 
 
 
